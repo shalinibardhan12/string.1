406655b50e985f2e792b2bc6d0b7077f42124c48
shalinibardhan12 <123828245+shalinibardhan12@users.noreply.github.com>
2023-03-10 21:50:40 +0530

    Add files via upload

---
+++ FirstST.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char name[20];
+    cout<<"enter name in string"<<endl;
+    cin>>name;
+    cout<<"your name is "<<name;
+    return 0;
+}
